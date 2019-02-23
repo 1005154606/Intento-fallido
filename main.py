29b2b10f9c267c2551f9df0e9ac9eefb1d03c7a3
unknown <wilson.escobar.2019@upb.edu.co>
2019-02-22 19:37:38 -0500

    Mi primera modificación

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 a=int(input("Introduce el primer número: "))
 c=int(input("Introduce el segundo número: "))
 producto=a*c
-print("el producto es", producto)
+print("el producto de", a ,"y", c ,"es", producto)
 doble_a=2*a
-print("el doble del primer número es", doble_a)
+print("el doble de", a , "es", doble_a)
+b=int(input("Introduce el cuarto número: "))
+cuadrado_b=b**2
+print("El cuadrado de", b , "es" ,cuadrado_b)
+import math
+d=int(input("Ingrese el quinto número: "))
+raiz_d=math.sqrt(d)
+print("la raiz cuadrada de", d ,"es", raiz_d)
